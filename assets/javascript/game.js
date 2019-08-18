4dcb73375dc0a20c7763a94cf409b467c39403d5
Chase Cannon <chase27cannon52@gmail.com>
2019-08-17 23:08:13 -0500

    3rd commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,4 +24,54 @@ $(document).ready(function() {
             if(!found)numbers[numbers.length]=randomNumber;
           }
         //console.log(newCrystals);		
-        
+        for (i = 0; i < numbers.length; i++) {
+          var imageCrystal = $('<img>');
+          imageCrystal.attr('data-num', numbers[i]);
+          imageCrystal.attr('src', crystals[i]);
+          imageCrystal.attr('alt', 'crystals');
+          imageCrystal.addClass('crystalImage')
+          $('#crystals').append(imageCrystal);
+        }
+      }
+    
+      function newGame() {
+    
+        counter = 0;
+        $('#yourScore').text(counter);
+    
+        function randomIntFromInterval(min,max){
+             return Math.floor(Math.random()*(max-min)+1);
+          }
+    
+        var numberToGuess = randomIntFromInterval(19,120);
+    
+        $('.value').text(numberToGuess);
+    
+    
+        $('.crystalImage').on('click', function(){
+            counter = counter + parseInt($(this).data('num'));
+           
+            $('#yourScore').text(counter);
+    //determine user's W's
+            if (counter == numberToGuess){
+              $('#status').text('You won.');
+              wins ++;
+              $('#win').text(wins);
+              console.log(wins)
+              $('#crystals').empty();
+              newCrystals();
+              newGame();
+      //determine user L's	        
+            } else if ( counter > numberToGuess){
+                $('#status').text('You have failed.');
+                losses ++;
+                $('#loss').text(losses);
+                console.log(losses)
+                $('#crystals').empty();
+                newCrystals();
+                newGame();
+            }
+        });
+      }
+    
+    });
